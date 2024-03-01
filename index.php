dcd4038c2329727d307abd38f5eef4f23fb56014
mpkelly <kikemelly@gmail.com>
2024-03-01 13:00:09 +0700

    Add PHPSniffer and PHPUnit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: My Cool Plugin
+ *
+ * @category Category
+ * @package  Mycoolpluin
+ * @author   John Smith <jsmith@email.com>
+ * @link     https://github.com/mpkelly/githubdeployments-phpunit-phpcs-example
+ */
+
+echo 'Hello, World';
